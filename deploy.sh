bb1ff859a8d222d45e9e1132c84ee7543aa33eb1
root <kedmenec.roberto@gmail.com>
2019-07-09 15:48:58 +0200

    modified:   deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,8 @@ git push deploy master
 
 ssh root@$IP -p $PORT <<EOF
 	echo "test3" >> test3.txt
+	ls /root/BLDB-test > dockerps.txt
+	cd /root/BLDB-test
         docker build -t "bldb-test" .
 	docker run -d --name "bldb-test" -p 9000:9000 bldb-test
 	docker ps -a > dockerps.txt
