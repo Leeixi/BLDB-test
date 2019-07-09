b9870a307ffe56f794efd8ae0c1bab3971145702
David <david.lee555@hotmail.com>
2019-07-09 14:46:45 +0200

    new file:   .gitignore 	modified:   .travis.yml 	modified:   deploy.sh 	modified:   id_rsa.enc

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,12 @@
-eval "$(ssh-agent -s)" # Start ssh-agent cache
-chmod 600 id_rsa # Allow read access to the private key
-ssh-add id_rsa # Add the private key to SSH
+eval "$(ssh-agent -s)"
+chmod 600 id_rsa
+ssh-add id_rsa
 
-git config --global push.default matching
-git remote add deploy ssh://root@$IP:$PORT$DEPLOY_DIR
+git config --global push.default matching 
+git remote add deploy ssh://root@$IP$PORT$DEPLOY_DIR
 git push deploy master
 
-# Skip this command if you don't need to execute any additional commands after deploying.
-ssh  root@$IP -p $PORT <<EOF
-  echo "tst je presel" >> /root/test.txt
-    crystal build --release --no-debug index.cr # Change to whatever commands you need!
-    EOF
+ssh root@$IP -p $PORT <<EOF
+	echo "test3" >> test3.txt
+EOF
+
